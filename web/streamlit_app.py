c5e0d6ca65e8af910a721a1a6aa899eaee72c9e3
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-23 15:55:49 +0800

    Confusion matrix opening

--- web/streamlit_app.py
+++ web/streamlit_app.py
@@ -1,2 +1,14 @@
 import streamlit as st
 #hilo
+st.title("SIC AR112 - Identification of Eye Disease using retinal fundus images")
+# Load image from URL
+url = "https://raw.githubusercontent.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/main/Confusion%20Matrix%20Accuracy%20Guarantee/Guarantee.png"
+response = requests.get(url)
+# Check if the request was successful
+if response.status_code == 200:
+    image = Image.open(BytesIO(response.content))
+
+    # Display the image using Streamlit
+    st.image(image, caption="Image from URL", use_column_width=True)
+else:
+    st.error(f"Failed to load image. Status code: {response.status_code}")  # Display an error message
