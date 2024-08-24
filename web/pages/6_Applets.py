4025dfddaffb677b4651f58a12cb848a5fbf7e3e
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-25 00:35:15 +0800

    Last edit???

--- web/pages/6_Applets.py
+++ web/pages/6_Applets.py
@@ -128,6 +128,32 @@ class CustomResNet(nn.Module):
 
         return x
 
+class ResidualBlock(nn.Module):
+
+    expansion = 1  # Set to 1 for basic ResNet blocks
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample:
+            residual = self.downsample(x)
+
+        out += residual
+        out = F.relu(out)
+        return out
 
 
 # Load the pre-trained ResNet model
