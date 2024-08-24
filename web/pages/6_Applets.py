5ddbb9f5c60b4f05320a519fb8045a2c0c28fe9f
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-25 00:25:01 +0800

    Putting in all the actual legit classes

--- web/pages/6_Applets.py
+++ web/pages/6_Applets.py
@@ -18,6 +18,8 @@ def load_labels(label_file):
 
 paths = ["models/resnet50.pth", "models/resnet34.pth", "models/ResNet-AR112.pth"]
 
+
+#Apparently required classes
 class EnsembleModel(nn.Module):
     def __init__(self, models, class_weights=None):
         super(EnsembleModel, self).__init__()
@@ -45,12 +47,87 @@ class EnsembleModel(nn.Module):
         final_output = torch.mean(stacked_outputs, dim=0)
         return final_output
 
-# Example usage:
-# Assuming you have 4 classes and 3 models
-# Example class-specific weights for each model:
-class_weights_model1 = [0.9241706161,	0.8933649289,	0.8672985782,	0.8317535545]
-class_weights_model2 = [0.9573459716,	0.9928909953,	0.9194312796,	0.9265402844]
-class_weights_model5 = [0.8744075829,	0.86492891,	0.691943128,	0.7772511848]
+class ReSNeT18(nn.Module):
+    def __init__(self, num_classes):
+        super(ReSNeT18, self).__init__()
+        self.model = models.resnet18(pretrained=True)
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+class ReSNeT34(nn.Module):
+    def __init__(self, num_classes):
+        super(ReSNeT34, self).__init__()
+        self.model = models.resnet34(pretrained=True)
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+class ReSNeT50(nn.Module):
+    def __init__(self, num_classes):
+        super(ReSNeT50, self).__init__()
+        self.model = models.resnet50(pretrained=True)
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+class CustomResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        super(CustomResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
 
 
 # Load the pre-trained ResNet model
