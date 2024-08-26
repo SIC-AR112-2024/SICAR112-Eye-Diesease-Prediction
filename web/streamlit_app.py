12e3d926916f1b411c1598e9b421ca502f4ef6db
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-26 13:18:04 +0800

    Remove error

--- web/streamlit_app.py
+++ web/streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.setPageConfig("Homepage")
 st.title("Home Page")
 st.write("Welcome to the home page of our SIC Project - AR112!")
 st.write("The goal of this project is to test the effectiveness of Large Language Models (LLMs) at identifiying eye diseases from retinal fundus images in comparison to traditional machine learning (ML) models like ResNets.")
