16c61828882924736592e35cf10ebf753ac48063
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-26 11:54:27 +0800

    Collab notebook update

--- web/pages/8_Acknowledgements_and_Reflections.py
+++ web/pages/8_Acknowledgements_and_Reflections.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.title("Last words")
-st.write("This is the contact page.")
+
+st.subheader("Sources / Software")
+st.write("We utilised a dataset we found on Kaggle, while our coding was carried out on Google Colab and VSCode. Our assets and code files are stored in Github, and we used Git and Git LFS to store our model .pth files in our Github.")
+
+st.subheader("Acknowledgements")
+st.write("We would like to thank our mentor, Mr Tan Pang Jin, for the guidance and help he has provided to us throughout this project.")
