0d5ae751c26bae0be2f0da8bfcb12e6047529c50
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-26 23:35:30 +0800

    Yes

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,3 +1,4 @@
+'''
 import streamlit as st
 #home page
 st.set_up_config(page_icon='web/more_images/Gap_Sem_Logo.png')
@@ -5,3 +6,4 @@ st.image('web/more_images/Gap_Sem_Logo.png')
 st.title("SIC AR112 2024 Landing Page")
 st.write("Welcome to the home page of our SIC Project - AR112!")
 st.write("The goal of this project is to test the effectiveness of Large Language Models (LLMs) at identifiying eye diseases from retinal fundus images in comparison to traditional machine learning (ML) models like ResNets.")
+'''
