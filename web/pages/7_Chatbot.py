b3b4bdaca2de2963ae760b992e835319dcc3dc1a
stonkmem <73430589+stonkmem@users.noreply.github.com>
2024-08-26 13:22:54 +0800

    streaming

--- web/pages/7_Chatbot.py
+++ web/pages/7_Chatbot.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import openai
+from time import sleep
+images = {
+    'Glaucoma':'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true', #Glaucoma
+    'Diabetic Retinopathy':'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true', #Diabetic Retinopathy
+    'Cataract':'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true'} #Cataract
+message = []
+
+def hello_my_name_is_markiplier(text):
+    for line in text.split('\n'):
+        for word in text.split(' '):
+            yield word + ' '
+            sleep(0.05)
+        yield '\n'
+
+
 st.title("LLM Playground")
 st.markdown("""Below, we have the ability to query GPT-4o using 0-shot and few-shot CoT prompting.
 For more information on how prompting helps LLMs, visit the corresponding pages in the sidebar.""")
@@ -7,12 +22,6 @@ ailment = st.multiselect("Pick a disease to diagnose:", ['Glaucoma', 'Diabetic R
 LLM_mode = st.multiselect("Pick a prompting method:", ['0-shot', 'Few-shot'])
 API_Key = st.text_input("API Key here 👇", placeholder="Type API Key (Ask us for ours!)")
 
-images = [
-    'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true', #Glaucoma
-    'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true', #Diabetic Retinopathy
-    'https://github.com/SIC-AR112-2024/SICAR112-Eye-Diesease-Prediction/blob/main/dataset/diabetic_retinopathy/342.jpg?raw=true'] #Cataract
-
-message = []
 if LLM_mode == '0-shot':
     message = [
     {'role':'user',
@@ -20,7 +29,7 @@ if LLM_mode == '0-shot':
         {'type': 'text', 'text':'''You are a medical student. You will be given several retinal fundus images as a test.
 Firstly, describe key features depicted in the image, of no less than 100 words, such as the macula, optic nerve, optic cup and disc and retinal blood discs.
 If the eye is healthy, say "HEALTHY". If not, tell me whether the patient has "CATARACT", "DIABETIC RETINOPATHY", or "GLAUCOMA". Your final diagnosis must be strictly 1 or 2 words, on a new line.'''},
-        {'type': 'image_url', 'image_url':''}
+        {'type': 'image_url', 'image_url':{'url':images[ailment]}}
     ]}]
 if LLM_mode == 'Few-shot':
     message = [
@@ -51,9 +60,26 @@ DIABETIC RETINOPATHY."""
             },
         ],
     },
-    {
-    "role": "assistant",
+    {"role": "assistant",
     "content": """The image shows a view of the fundus of the eye. The optic disc, the point where the optic nerve leaves the eye, is visible in the center of the image. The disc is slightly pale and the margins appear blurred. The macula, the central region responsible for central vision, appears slightly edematous. The retinal blood vessels are clearly visible and appear normal.
-GLAUCOMA"""
-    }
-]
+GLAUCOMA"""}]
+    message.append({"role": "user",
+            "content": [
+                {"type": "text", "text": "Produce a diagnosis for the following:"},
+                {"type": "image_url", "image_url": {
+                    "url": images[ailment]
+    }}]})
+
+for i in message:
+    if i['role'] == 'user':
+        with st.chat_message('user'):
+            st.write(i['content'][0]['text'])
+            st.image(i['content'][1]['image_url']['url'])
+    elif i['role'] == 'system':
+        with st.chat_message('System Instructions'):
+            st.write(i['content'])
+    else:
+        with st.chat_message('ai'):
+            st.write(i['content'])
+
+gen = 
