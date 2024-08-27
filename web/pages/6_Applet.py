4a2d54fe6cdfe6ee84c8eba917937bdf6f609c2c
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-27 12:41:06 +0800

    Pushy

--- web/pages/6_Applet.py
+++ web/pages/6_Applet.py
@@ -358,7 +358,8 @@ if uploaded_file is not None:
             # Get explanations for each prediction
             counter = Counter(predicted_labels)
             most_common_element, count = counter.most_common(1)[0]
-            explanation_condensed = get_explanation(encode_image(uploaded_file), most_common_element)
+            image_content = encode_image(uploaded_file)
+            explanation_condensed = get_explanation(image_content, most_common_element)
             
             # Display explanations
             st.write(f"Explanation: {explanation_condensed}")
