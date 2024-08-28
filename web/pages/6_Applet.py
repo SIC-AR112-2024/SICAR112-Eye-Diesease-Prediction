0a3c83ebc3a4fbc8a63f99f25fbe24039da57e90
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-28 13:11:48 +0800

    Change Image size

--- web/pages/6_Applet.py
+++ web/pages/6_Applet.py
@@ -289,7 +289,7 @@ if uploaded_file is not None:
     #st.image(image, caption="Retinal Fundus Image", use_column_width=True)
     
     # Resize the image
-    resized_image = image.resize((int(image.width/10), int(image.height/10)))
+    resized_image = image.resize((int(image.width/9), int(image.height/9)))
     
     # Display the resized image
     st.image(resized_image, caption="Retinal Fundus Image", use_column_width=False)
