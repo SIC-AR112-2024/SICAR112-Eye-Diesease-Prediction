b97a37d9d2041fb1f9ef73dfd9352f53db7b53df
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-28 17:21:58 +0800

    Adding a bit more to 0 shot

--- web/pages/4_0-Shot_LLM.py
+++ web/pages/4_0-Shot_LLM.py
@@ -7,3 +7,6 @@ st.write("Recent advances in LLM technology has allowed some LLMs to have vision
 st.subheader("About Zero-shot learning")
 st.write("Zero-shot learning refers to the model being given images not shown during training and classifying them. In this case, we used a LLM model with vision capabilities, and gave it a small testing dataset of retinal fundus images to gauge its proficiency in diagnosing the diseases.")
 st.write("The model we used was Chatgpt-4o, with temperature set to 0 (same for few-shot learning as well). Temperature is a parameter that influences how creative it is, and in this case we need the outputs to be logical and predictable, so a low temperature is required. In the prompt, the model was instructed to only respond with the name of the disease for easy validation.")
+st.subheader("Limitations of Zero-shot learning")
+st.write("Due to the lack of exposure to the images, the machine is prone to misinterpreting the image and giving an incorrect diagnosis. This is especially apparent in eye disease detection due to the similarities between the two images (all are retinal fundus images and look generally similar).")
+st.write("As the machine does not have a reference prompt template, the machine may not generate the prompt in the format requested by the user, which may pose problems in understanding and explainability.")
