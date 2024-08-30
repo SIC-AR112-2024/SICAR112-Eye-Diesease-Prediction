694bc1b20f57fbb260bcb0df02c4b4f3588d1a64
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-30 19:55:47 +0800

    iframe

--- web/pages/10_Video_Walkthrough.py
+++ web/pages/10_Video_Walkthrough.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
 # Embed a video from a URL (e.g., YouTube)
-video_url = f"https://drive.google.com/uc?export=download&id=1s5AbPg4iZJh8GhAEPmy1Fa7VGVeWSVWn"
+video_id = "1s5AbPg4iZJh8GhAEPmy1Fa7VGVeWSVWn"
 
 
+# Construct the iframe code
+iframe_code = f"""
+<iframe src="https://drive.google.com/file/d/{video_id}/preview" width="640" height="480"></iframe>
+"""
 st.header('Video Walkthrough')
 # Embed the video
 st.write('Watch our website walkthrough video guide narrated by Artificial Intelligence 😊.')
-st.video(video_url)
+# Embed the iframe in Streamlit
+st.markdown(iframe_code, unsafe_allow_html=True)
+
+
