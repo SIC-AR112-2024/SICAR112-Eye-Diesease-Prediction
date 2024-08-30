f8ca2c10288b702d167cfc5e8ef28c96795f82d5
SIC-AR112-2024 <ar112sic@gmail.com>
2024-08-30 19:47:13 +0800

    test video

--- web/pages/10_Video_Walkthrough.py
+++ web/pages/10_Video_Walkthrough.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 # Embed a video from a URL (e.g., YouTube)
 video_url = "https://drive.google.com/file/d/1s5AbPg4iZJh8GhAEPmy1Fa7VGVeWSVWn/view?usp=sharing"
+test_vid = "https://www.youtube.com/watch?v=iBuTEywEQ6U"
 
 st.header('Video Walkthrough')
 # Embed the video
 st.write('Watch our website walkthrough video guide narrated by Artificial Intelligence 😊.')
-st.video(video_url)
+st.video(test_vid)
