d512b912b8a9a7ffbefe46a63971ef7e9ddb68d9
SIC-AR112-2024 <ar112sic@gmail.com>
2024-09-28 01:20:46 +0800

    Trialling

--- web/pages/7_Chatbot.py
+++ web/pages/7_Chatbot.py
@@ -10,6 +10,7 @@ import os
 API_KEY = os.getenv("OPENAI_API_KEY")
 if API_KEY is not None:
     print("Secret Key Retrieved Successfully")
+    openai.api_key = API_KEY
 else:
     print("Failed to retrieve the Secret Key")
 
