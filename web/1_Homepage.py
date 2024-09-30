dee36609180eae04876e3b24bb2bd4d7a56f91cf
SIC-AR112-2024 <ar112sic@gmail.com>
2024-09-30 21:20:51 +0800

    Names ins

--- web/1_Homepage.py
+++ web/1_Homepage.py
@@ -16,6 +16,7 @@ resized_image = image.resize((int(image.width/3.5), int(image.height/3.5)))
 st.image(resized_image, use_column_width=False)
 #st.image('web/more_images/Logo_Image.png')
 st.title("Analysing and Building Risk Prediction Frameworks on Retinal Fundus Images")
+st.write("By: Lee Zhe Yu, Nathan (26Y 4E), Chew Yee (26Y 4E) and Swaagat Bikash Saikia (26Y 4E)")
 st.write("Welcome to the home page of our SIC Project - AR112!")
 
 st.subheader("Introduction")
